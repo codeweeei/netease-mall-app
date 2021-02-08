a397386bbb731d2cad8f76d725aa76c6f63ea1ab
王威 <1749894019@qq.com>
2021-02-08 17:45:54 +0800

    引入vant

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Home"
+}
+</script>
+
+<style scoped>
+
+</style>
